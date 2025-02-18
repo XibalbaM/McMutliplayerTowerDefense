cbe4cf245dacf647518d44da696a32e9db18d7c8
xibalbam <maelporret@outlook.fr>
2025-02-18 21:41:08 +0100

    Initial commit

---
+++ src/client/kotlin/MultiTowerDefenseClient.kt
@@ -0,0 +1,9 @@
+package fr.xibalba.minecraft.mutliplayertowerdefense.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class MultiTowerDefenseClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/MultiTowerDefense.kt
@@ -0,0 +1,9 @@
+package fr.xibalba.minecraft.mutliplayertowerdefense
+
+import net.fabricmc.api.ModInitializer
+
+class MultiTowerDefense : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
